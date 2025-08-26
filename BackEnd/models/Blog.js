7f29caa282318ef0534d33589500d827922e7824
Shandeep <shandeepsanju@gmail.com>
2025-08-26 22:40:56 +0530

    m

--- BackEnd/models/Blog.js
+++ BackEnd/models/Blog.js
@@ -19,7 +19,6 @@ const blogSchema = new mongoose.Schema({
   type: String
 }, { timestamps: true })
 
-
 // OPTIONAL: prevent duplicates when reference_id is provided
 blogSchema.index({ reference_id: 1 }, { unique: true, sparse: true })
 
